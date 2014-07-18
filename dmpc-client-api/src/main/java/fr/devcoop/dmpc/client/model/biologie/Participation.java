1099bf66bb3441903cf5560684d91ed8e7a55893
lforet <laurent.foret@gmail.com>
2014-07-18 17:07:06 +0200

    add commentary.

--- dmpc-client-api/src/main/java/fr/devcoop/dmpc/client/model/biologie/Participation.java
+++ dmpc-client-api/src/main/java/fr/devcoop/dmpc/client/model/biologie/Participation.java
@@ -3,7 +3,12 @@ package fr.devcoop.dmpc.client.model.biologie;
 import fr.devcoop.dmpc.client.model.DMPCPersonnelSante;
 
 /**
- *
+ * Une participation permet de définir l'action d'un utilisateur danns une période donnée.
+ * 
+ * Pour une participation d'un Personnel de Santé, il est possible de définir 
+ *   <li>un instant : en renseignant le stopTime uniquement,
+ *   <li>une période : en reseignant le startTime ainsi que le stopTime.
+ * 
  * @author lfo
  */
 public class Participation {
